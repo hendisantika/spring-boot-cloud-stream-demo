3294634360d64094d1592b3789652243168b65ad
hendisantika <hendisantika@yahoo.co.id>
2024-06-18 10:35:53 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/cloudstreamdemo/SpringBootCloudStreamDemoApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.cloudstreamdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudStreamDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCloudStreamDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/cloudstreamdemo/SpringBootCloudStreamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.cloudstreamdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudStreamDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
